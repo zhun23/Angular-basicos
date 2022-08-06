ea1c84f0514dc8792d58bbef9bb559f3a9c4d010
Ivan Casado <ivanc.bcn@gmail.com>
2022-08-06 14:08:00 +0200

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component, forwardRef } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+
+heroes: string [] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitán América'];
+  heroeBorrado: string = '';
+
+  borrarHeroe(): void {
+    this.heroeBorrado = this.heroes.shift() || '';
+    console.log (this.heroeBorrado);
+
+  }
+}
